ffb378cb2b99350147e1d818aae3160a0826d003
Aki Van Ness <aki@lethalbit.net>
2022-11-26 09:27:27 -0500

    torii: platform: initial draft of OpenLANE based  ASIC support

--- torii/platform/vendor/__init__.py
+++ torii/platform/vendor/__init__.py
@@ -4,6 +4,7 @@ from .intel               import IntelPlatform
 from .lattice_ecp5        import LatticeECP5Platform
 from .lattice_ice40       import LatticeICE40Platform
 from .lattice_machxo_2_3l import LatticeMachXO2Platform, LatticeMachXO3LPlatform
+from .openlane            import OpenLANEPlatform
 from .quicklogic          import QuicklogicPlatform
 from .xilinx              import XilinxPlatform
 
@@ -14,6 +15,8 @@ __all__ = (
 	'LatticeICE40Platform',
 	'LatticeMachXO2Platform',
 	'LatticeMachXO3LPlatform',
+
+	'OpenLANEPlatform',
 
 	'QuicklogicPlatform',
 
