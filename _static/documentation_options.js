755b9cf94ec1909f45f6c89bc46a9c838f6b04b6
lethalbit <201479+lethalbit@users.noreply.github.com>
2023-02-16 02:00:47 +0000

    Deploying to gh-pages from @ shrine-maiden-heavy-industries/torii-hdl@cb8c1c6f43f5b4c6c9364acca857adf0569b444d 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.4.dev57',
+    VERSION: '0.4.4.dev60',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
